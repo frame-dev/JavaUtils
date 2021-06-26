a8ed77206630f4751f0ce9c520749255c0da0e43
frame-dev <darrylhuber39@gmail.com>
2021-06-26 19:52:42 +0200

    FileDownloader Added

---
+++ src/main/java/de/framedev/javautils/ReplaceList.java
@@ -0,0 +1,29 @@
+package de.framedev.javautils;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ * / This Plugin was Created by FrameDev
+ * / Package : de.framedev.javautils
+ * / ClassName CustomString
+ * / Date: 17.06.21
+ * / Project: JavaUtils
+ * / Copyrighted by FrameDev
+ */
+
+public class ReplaceList<E> extends ArrayList<E> implements Serializable {
+
+    /**
+     * Replace Data
+     **
+     * @param oldData the OldData for Replace
+     * @param newData the new Data to Replace with the oldData
+     */
+    public void replace(E oldData, E newData) {
+        if (contains(oldData)) {
+            set(indexOf(oldData), newData);
+        }
+    }
+
+}
