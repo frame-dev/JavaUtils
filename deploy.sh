7391520be68a1596d910b39a0b6775a1f3ccea8d
frame-dev <darrylhuber39@gmail.com>
2024-01-30 21:31:37 +0100

    Updated

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-echo "Maven Clean has been Started!"
-mvn clean
-echo "Maven Clean Finished"
-echo "Maven Package Started"
-mvn package
-echo "Maven Package Finished"
-echo "Maven Deploy Started!"
-mvn deploy
-echo "Maven Deploy Finished"
